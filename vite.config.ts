012ce4f79e9fb2d1c6748b8ad37a8a9e444ed416
roshani0331 <roshanisoni095s@gmail.com>
2025-08-02 20:14:47 +0530

    Fixing build issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// Hotfix: Patch crypto for Node 20
-if (!globalThis.crypto?.subtle) {
-  const { webcrypto } = await import('node:crypto');
-  (globalThis as any).crypto = webcrypto;
-}
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+})
